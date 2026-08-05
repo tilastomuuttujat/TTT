8967e7946d35ac290734da080a78d9d0c8dbb399
tilastomuuttujat <55614962+tilastomuuttujat@users.noreply.github.com>
2026-08-05 21:03:17 +0300

    Add Atlas UI component bundle

---
+++ atlas/components/atlas-ui.js
@@ -0,0 +1,13 @@
+import "./atlas-stat-card.js";
+import "./atlas-article.js";
+import "./atlas-topic.js";
+import "./atlas-timeline.js";
+import "./atlas-network.js";
+
+export const ATLAS_COMPONENTS = [
+  "atlas-stat-card",
+  "atlas-article",
+  "atlas-topic",
+  "atlas-timeline",
+  "atlas-network",
+];
