160e2a1c9d634d8bfd5400762c74bc90062aeb71
tilastomuuttujat <55614962+tilastomuuttujat@users.noreply.github.com>
2026-08-05 22:33:27 +0300

    Register Atlas matrix component in UI Kit

--- atlas/components/atlas-ui.js
+++ atlas/components/atlas-ui.js
@@ -3,6 +3,7 @@ import "./atlas-article.js";
 import "./atlas-topic.js";
 import "./atlas-timeline.js";
 import "./atlas-network.js";
+import "./atlas-matrix.js";
 
 export const ATLAS_COMPONENTS = [
   "atlas-stat-card",
@@ -10,4 +11,5 @@ export const ATLAS_COMPONENTS = [
   "atlas-topic",
   "atlas-timeline",
   "atlas-network",
+  "atlas-matrix",
 ];
